aba33e1fbc63c55688265b791860be7f85a9b508
NangKhatMang <nkm.nangkhatmang@gmail.com>
2025-10-16 16:21:12 +0630

    update: patchwork logo change

--- app/helpers/branding_helper.rb
+++ app/helpers/branding_helper.rb
@@ -11,14 +11,14 @@ module BrandingHelper
   end
 
   def _logo_as_symbol_wordmark
-    content_tag(:svg, tag.use(href: '#logo-symbol-wordmark'), viewBox: '0 0 261 66', class: 'logo logo--wordmark')
+    image_tag(frontend_asset_path('images/patchwork-with-text.svg'), alt: 'Patchwork Logo', width: 100, class: 'logo logo--wordmark')
   end
 
   def _logo_as_symbol_icon
-    content_tag(:svg, tag.use(href: '#logo-symbol-icon'), viewBox: '0 0 79 79', class: 'logo logo--icon')
+    image_tag(frontend_asset_path('images/patchwork-logo.svg'), alt: 'Patchwork Logo', class: 'logo logo--icon')
   end
 
   def render_logo
-    image_tag(frontend_asset_path('images/logo.svg'), alt: 'Mastodon', class: 'logo logo--icon')
+    image_tag(frontend_asset_path('images/patchwork-logo.svg'), alt: 'Patchwork Logo', class: 'logo logo--icon')
   end
 end
